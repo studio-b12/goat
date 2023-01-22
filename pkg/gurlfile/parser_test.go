2a55d0ebfcb1ffa86f1fad43bf97be6a7d1b9e50
zekroTJA <riho@live.de>
2023-01-22 10:22:56 +0000

    add more unit tests [#1]

--- pkg/gurlfile/parser_test.go
+++ pkg/gurlfile/parser_test.go
@@ -270,6 +270,480 @@ some-key-2:
 	})
 }
 
+func TestParse_BlockRaw(t *testing.T) {
+	t.Run("unescaped-empty", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, []byte(nil), res.Tests[0].Body)
+	})
+
+	t.Run("unescaped-EOF", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+some body content
+some more content
+`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("unescaped-newblock", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+some body content
+some more content
+
+[QueryParams]
+`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("unescaped-newrequest", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+some body content
+some more content
+
+---
+`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("unescaped-section", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+some body content
+some more content
+
+### tests
+`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("escaped-empty", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+´´´
+`
+
+		p := stringParser(swapTicks(raw))
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, []byte(nil), res.Tests[0].Body)
+	})
+
+	t.Run("escaped-EOF", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+some body content
+some more content
+´´´
+`
+
+		p := stringParser(swapTicks(raw))
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("escaped-newblock", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+some body content
+
+[QueryParams]
+some more content
+´´´
+
+[QueryParams]
+`
+
+		p := stringParser(swapTicks(raw))
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\n\n[QueryParams]\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("escaped-newrequest", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+some body content
+
+---
+
+some more content
+´´´
+
+---
+`
+
+		p := stringParser(swapTicks(raw))
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\n\n---\n\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("escaped-section", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+some body content
+
+### setup
+
+some more content
+´´´
+
+### tests
+`
+
+		p := stringParser(swapTicks(raw))
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t,
+			[]byte("some body content\n\n### setup\n\nsome more content\n"),
+			res.Tests[0].Body)
+	})
+
+	t.Run("escaped-open", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[Body]
+´´´
+some body content
+
+---
+`
+
+		p := stringParser(swapTicks(raw))
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrOpenEscapeBlock, err)
+	})
+}
+
+func TestParse_BlockValues(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+		
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-strings", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+string1 = "some string 1"
+string2 =     "some string 2"
+string3 = 		"some string 3" 
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{
+			"string1": "some string 1",
+			"string2": "some string 2",
+			"string3": "some string 3",
+		}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-integer", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+int1 = 1
+int2 = 1_000
+int3 = -123
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{
+			"int1": int64(1),
+			"int2": int64(1000),
+			"int3": int64(-123),
+		}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-float", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+float1 = 1.234
+float2 = 1_000.234
+float3 = 0.12
+float4 = -12.34
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{
+			"float1": float64(1.234),
+			"float2": float64(1000.234),
+			"float3": float64(0.12),
+			"float4": float64(-12.34),
+		}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-boolean", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+bool1 = true
+bool2 = false
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{
+			"bool1": true,
+			"bool2": false,
+		}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-array", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+arrayEmpty1 = []
+arrayEmpty2 = [  	]
+
+arrayString1 = ["some string"]
+arrayString2 = ["some string", "another string","and another one"]
+
+arrayInt1 = [1]
+arrayInt2 = [1, 2,-3,	4_000]
+
+arrayFloat1 = [1.23]
+arrayFloat2 = [1.0, -1.1,1.234]
+
+arrayMixed = ["a string", 2, 3.456, true]
+
+arrayNested = [[1,2], [[true, false], "foo"]]
+
+arrayMultiline = [
+	"foo",
+	"bar"
+]
+
+arrayLeadingComma1 = [true, false,]
+arrayLeadingComma2 = [
+	true, 
+	false,
+]
+		`
+
+		p := stringParser(raw)
+		res, err := p.Parse()
+
+		assert.Nil(t, err, err)
+		assert.Equal(t, map[string]any{
+			"arrayEmpty1":        []any(nil),
+			"arrayEmpty2":        []any(nil),
+			"arrayString1":       []any{"some string"},
+			"arrayString2":       []any{"some string", "another string", "and another one"},
+			"arrayInt1":          []any{int64(1)},
+			"arrayInt2":          []any{int64(1), int64(2), int64(-3), int64(4_000)},
+			"arrayFloat1":        []any{1.23},
+			"arrayFloat2":        []any{1.0, -1.1, 1.234},
+			"arrayMixed":         []any{"a string", int64(2), 3.456, true},
+			"arrayNested":        []any{[]any{int64(1), int64(2)}, []any{[]any{true, false}, "foo"}},
+			"arrayMultiline":     []any{"foo", "bar"},
+			"arrayLeadingComma1": []any{true, false},
+			"arrayLeadingComma2": []any{true, false},
+		}, res.Tests[0].QueryParams)
+	})
+
+	t.Run("value-invalid-entry", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+invalid
+		`
+
+		p := stringParser(raw)
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrInvalidBlockEntryAssignment, err)
+	})
+
+	t.Run("value-invalid-assignment", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+invalid = 
+		`
+
+		p := stringParser(raw)
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrInvalidToken, err)
+	})
+
+	t.Run("value-invalid-string", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+invalid = "
+		`
+
+		p := stringParser(raw)
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrInvalidToken, err)
+	})
+
+	t.Run("value-invalid-array-1", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+invalid = [
+		`
+
+		p := stringParser(raw)
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrInvalidToken, err)
+	})
+
+	t.Run("value-invalid-array-2", func(t *testing.T) {
+		const raw = `
+		
+GET https://example.com
+
+[QueryParams]
+invalid = [1, 2
+		`
+
+		p := stringParser(raw)
+		_, err := p.Parse()
+
+		assert.ErrorIs(t, err, ErrInvalidToken, err)
+	})
+}
+
+// --- Helpers --------------------------------------------
+
 func stringParser(raw string) *Parser {
 	return NewParser(strings.NewReader(raw))
+}
+
+func swapTicks(v string) string {
+	return strings.ReplaceAll(v, "´", "`")
 }
