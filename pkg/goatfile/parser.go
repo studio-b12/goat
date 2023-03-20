4b1d5b5a8db0e13b264d8d1aa6d0bb12531bded4
zekroTJA <riho@live.de>
2023-03-20 09:49:02 +0000

    parser: deprecate  `[headers]` block name

--- pkg/goatfile/parser.go
+++ pkg/goatfile/parser.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/studio-b12/goat/pkg/errs"
+	"github.com/zekrotja/rogu/log"
 )
 
 // Parser parses a Goatfile.
@@ -263,7 +264,17 @@ func (t *Parser) parseBlock(req *requestParseChecker) error {
 		}
 		req.QueryParams = data
 
-	case optionNameHeader, optionNameHeaders:
+	case optionNameHeader:
+		err := t.parseHeaders(req.Header)
+		if err != nil {
+			return err
+		}
+	case optionNameHeaders:
+		// TODO: Remove due to deprecation
+		log.Warn().Tag("Parser").
+			Field("pos", fmt.Sprintf("%d:%d", t.s.line, t.s.linepos)).
+			Msg("Option name [headers] is deprecated! Please use [header] instead! See the release notes of v0.8.0 for more information: " +
+				"https://github.com/studio-b12/goat/releases/tag/v0.8.0")
 		err := t.parseHeaders(req.Header)
 		if err != nil {
 			return err
