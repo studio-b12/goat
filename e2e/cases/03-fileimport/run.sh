ed2431458c4aa54acf1e5ff03e05f59ef06dc051
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-06-29 11:15:47 +0200

    replace testfile line endings using sed

--- e2e/cases/03-fileimport/run.sh
+++ e2e/cases/03-fileimport/run.sh
@@ -1,6 +1,8 @@
 set -e
 
-dos2unix body.txt
+# CRLF to LF, because git on windows does git on windows things.
+# Also '-i' can not be used because sed on OSX does sed on SOX things.
+cat body.txt | sed -i 's/^M$//' > body.txt
 
 goat direct.goat
 goat imported.goat
