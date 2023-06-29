f0ea067827e4d8180862e1fe14f92a52b01f8776
Ringo Hoffmann <r.hoffmann@b12-gruppe.de>
2023-06-29 11:54:56 +0200

    fix sed replacement in e2e (please work now)

--- e2e/cases/03-fileimport/run.sh
+++ e2e/cases/03-fileimport/run.sh
@@ -2,7 +2,7 @@ set -e
 
 # CRLF to LF, because git on windows does git on windows things.
 # Also '-i' can not be used because sed on OSX does sed on SOX things.
-cat body.txt | sed -i 's/^M$//' > body.txt
+cat body.txt | sed 's/^M$//' | tee body.txt
 
 goat direct.goat
 goat imported.goat
