3664b17c51f61cef893fc06cd61988a1faa03fc6
Ringo Hoffmann <riho@live.de>
2024-06-15 08:30:51 +0200

    fix e2e tests on macOS

--- e2e/cases/fileimport-parameterized/run.sh
+++ e2e/cases/fileimport-parameterized/run.sh
@@ -2,7 +2,8 @@ set -e
 
 # CRLF to LF, because git on windows does git on windows things.
 # Also '-i' can not be used because sed on OSX does sed on SOX things.
-cat body.txt | sed 's/^M$//' | tee body.txt
+sed 's/^M$//' < body.txt > body.txt.tmp
+mv body.txt.tmp body.txt
 
 goat -a "file=body.txt" direct.goat
 goat -a "file=body.txt" imported.goat
